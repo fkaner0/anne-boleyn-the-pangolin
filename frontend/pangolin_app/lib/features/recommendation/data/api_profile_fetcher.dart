473c01125e25973b10ca6d7cbb0e133700a338d9
Charlie Shaw <charliecpshaw@gmail.com>
2026-05-28 18:35:41 +0100

    feat (recommendation): use API for recommendation instead of mocks [ccs24]

---
+++ frontend/pangolin_app/lib/features/recommendation/data/api_profile_fetcher.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:pangolin_app/features/recommendation/data/profile_fetcher.dart';
+import 'package:pangolin_app/features/recommendation/domain/profile.dart';
+
+class ApiProfileFetcher implements ProfileFetcher {
+  final String host;
+  final int? port;
+
+  const ApiProfileFetcher({this.host = 'localhost', this.port});
+
+  @override
+  Future<Profile> fetchProfile(int userId) async {
+    String baseUrl;
+    if (port == null) {
+      baseUrl = host;
+    } else {
+      baseUrl = '$host:$port';
+    }
+    final uri = Uri.https(baseUrl, '/profile/$userId');
+
+    final response = await http.get(uri);
+
+    if (response.statusCode != 200) {
+      throw Exception(
+        'Failed to fetch recommendations: ${response.statusCode}',
+      );
+    }
+
+    final decoded = jsonDecode(response.body);
+
+    return Profile.fromJson(decoded as Map<String, dynamic>);
+  }
+}
---
+++ frontend/pangolin_app/lib/features/recommendation/data/render_profile_fetcher.dart
@@ -0,0 +1,14 @@
+import 'package:pangolin_app/features/recommendation/data/api_profile_fetcher.dart';
+import 'package:pangolin_app/features/recommendation/data/profile_fetcher.dart';
+import 'package:pangolin_app/features/recommendation/domain/profile.dart';
+
+class RenderProfileFetcher implements ProfileFetcher {
+  final ApiProfileFetcher _delegate = const ApiProfileFetcher(
+    host: 'anne-boleyn-the-pangolin.onrender.com',
+  );
+
+  @override
+  Future<Profile> fetchProfile(int userId) {
+    return _delegate.fetchProfile(userId);
+  }
+}
