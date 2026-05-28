b6993e0b8e3e512b5cd75a25daeaf0a8246fc904
Mystrite <finleydenton@outlook.com>
2026-05-28 12:15:14 +0100

    chore: rename mock fetcher to MockRecommendationFetcher

--- frontend/pangolin_app/lib/main.dart
+++ frontend/pangolin_app/lib/main.dart
@@ -10,7 +10,7 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
   final RecommendationFetcher recommendationFetcher =
-      MockRecommendationRepository();
+      MockRecommendationFetcher();
 
   @override
   Widget build(BuildContext context) {
