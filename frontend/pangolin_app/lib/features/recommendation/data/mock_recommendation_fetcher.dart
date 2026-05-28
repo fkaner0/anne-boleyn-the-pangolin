dc7fd54150348877cf2920c01d475c9fb9d50bd7
Mystrite <finleydenton@outlook.com>
2026-05-28 11:22:01 +0100

    feat: add userId field to Recommendation class

--- frontend/pangolin_app/lib/features/recommendation/data/mock_recommendation_fetcher.dart
+++ frontend/pangolin_app/lib/features/recommendation/data/mock_recommendation_fetcher.dart
@@ -9,18 +9,21 @@ class MockRecommendationRepository implements RecommendationFetcher {
 
     return const [
       Recommendation(
+        userId: 0,
         name: 'Tim Johnson',
         location: 'Harrow, London',
         bio: 'Budding watercolour artist, been enjoying painting ponds.',
         imageUrl: 'https://via.placeholder.com/150',
       ),
       Recommendation(
+        userId: 1,
         name: 'Sally Parks',
         location: 'Hammersmith, London',
         bio: 'I love apples. I love still life. I love drawing apples in still life.',
         imageUrl: 'https://via.placeholder.com/150',
       ),
       Recommendation(
+        userId: 2,
         name: 'Selena Davis',
         location: 'Richmond, London',
         bio: 'Finger painting fanatic, check out my pangolin art.',
--- frontend/pangolin_app/lib/features/recommendation/domain/recommendation.dart
+++ frontend/pangolin_app/lib/features/recommendation/domain/recommendation.dart
@@ -1,10 +1,12 @@
 class Recommendation {
+  final int userId;
   final String name;
   final String location;
   final String bio;
   final String imageUrl;
 
   const Recommendation({
+    required this.userId,
     required this.name,
     required this.location,
     required this.bio,
@@ -13,6 +15,7 @@ class Recommendation {
 
   factory Recommendation.fromJson(Map<String, dynamic> json) {
     return Recommendation(
+      userId: json['userId'] as int,
       name: json['name'] as String,
       location: json['location'] as String,
       bio: json['bio'] as String,
