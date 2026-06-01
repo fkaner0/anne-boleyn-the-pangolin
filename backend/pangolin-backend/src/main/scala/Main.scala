a3064fd2dd339f08faf11fac3aecd72c67ae048b
Charlie Shaw <charliecpshaw@gmail.com>
2026-06-01 14:53:05 +0100

    style: formatting [ccs24]

--- backend/pangolin-backend/src/main/scala/Main.scala
+++ backend/pangolin-backend/src/main/scala/Main.scala
@@ -27,16 +27,38 @@ import org.postgresql.xa.PGXADataSource
 
 import com.augustnagro.magnum.*
 
-case class Recommendation(userId: Int, name: String, location: String, bio: String, profileImageUrl: String, rejected: Boolean)
+case class Recommendation(
+    userId: Int,
+    name: String,
+    location: String,
+    bio: String,
+    profileImageUrl: String,
+    rejected: Boolean
+)
 object Recommendation {
   given ReadWriter[Recommendation] = macroRW
 
   def fromProfile(profile: Profile): Recommendation = {
-    Recommendation(profile.userId, profile.name, profile.location, profile.bio, profile.profileImageUrl, profile.rejected)
+    Recommendation(
+      profile.userId,
+      profile.name,
+      profile.location,
+      profile.bio,
+      profile.profileImageUrl,
+      profile.rejected
+    )
   }
 }
 
-case class Profile(userId: Int, name: String, location: String, bio: String, profileImageUrl: String, imageUrls: List[String], rejected: Boolean)
+case class Profile(
+    userId: Int,
+    name: String,
+    location: String,
+    bio: String,
+    profileImageUrl: String,
+    imageUrls: List[String],
+    rejected: Boolean
+)
 object Profile {
   given ReadWriter[Profile] = macroRW
 }
@@ -45,9 +67,13 @@ val dataSource: javax.sql.DataSource = {
   val ds = PGSimpleDataSource()
   ds.setDatabaseName("pangolindb")
   ds.setUser("pangolindbuser")
-  ds.setPassword(sys.env.getOrElse("DB_PASSWORD", os.read(os.pwd / "db-password.txt")))
+  ds.setPassword(
+    sys.env.getOrElse("DB_PASSWORD", os.read(os.pwd / "db-password.txt"))
+  )
   ds.setPortNumber(5432)
-  ds.setUrl("jdbc:postgresql://dpg-d8cbgu3eo5us73eq2hl0-a.frankfurt-postgres.render.com")
+  ds.setUrl(
+    "jdbc:postgresql://dpg-d8cbgu3eo5us73eq2hl0-a.frankfurt-postgres.render.com"
+  )
   ds
 }
 
@@ -62,17 +88,18 @@ var tim = Profile(
   bio = "Budding watercolour artist, been enjoying painting ponds.",
   profileImageUrl = defaultImageUrl,
   imageUrls = List(defaultImageUrl),
-  rejected = false,
+  rejected = false
 )
 
 var sally = Profile(
   userId = 1,
   name = "Sally Parks",
   location = "Hammersmith, London",
-  bio = "I love apples. I love still life. I love drawing apples in still life.",
+  bio =
+    "I love apples. I love still life. I love drawing apples in still life.",
   profileImageUrl = defaultImageUrl,
   imageUrls = List(defaultImageUrl, defaultImageUrl),
-  rejected = false,
+  rejected = false
 )
 
 var selena = Profile(
@@ -82,47 +109,48 @@ var selena = Profile(
   bio = "Finger painting fanatic, check out my pangolin art.",
   profileImageUrl = "https://via.placeholder.com/150",
   imageUrls = List(defaultImageUrl, defaultImageUrl, defaultImageUrl),
-  rejected = false,
+  rejected = false
 )
 
 def profiles = Vector(tim, sally, selena)
-def recommendations = profiles.map(Recommendation.fromProfile).filter(!_.rejected)
+def recommendations =
+  profiles.map(Recommendation.fromProfile).filter(!_.rejected)
 
 object PangolinHttp4sServer extends IOApp {
 
-  val profileEndpoint = endpoint
-    .get
+  val profileEndpoint = endpoint.get
     .in("profile" / path[Int]("userId"))
     .out(jsonBody[Profile])
 
-  val reccomendationsEndpoint = endpoint
-    .get
+  val reccomendationsEndpoint = endpoint.get
     .in("recommendations")
     .out(jsonBody[Vector[Recommendation]])
 
-  val rejectProfileEndpoint = endpoint
-    .put
+  val rejectProfileEndpoint = endpoint.put
     .in("profile" / path[Int]("userId"))
     .in(jsonBody[Boolean])
 
-  given ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+  given ec: ExecutionContext =
+    scala.concurrent.ExecutionContext.Implicits.global
 
   val http4sOptions: Http4sServerOptions[IO] = Http4sServerOptions
     .customiseInterceptors[IO]
     .corsInterceptor(
       CORSInterceptor.customOrThrow(
-        CORSConfig.default
-          .allowAllHeaders
-          .allowAllOrigins
-          .allowAllMethods
+        CORSConfig.default.allowAllHeaders.allowAllOrigins.allowAllMethods
           .maxAge(42.seconds) // TODO
-        )
-      ).options
+      )
+    )
+    .options
 
   val serverInterpreter = Http4sServerInterpreter[IO](http4sOptions)
 
   val recommendationsRoutes: HttpRoutes[IO] =
-    serverInterpreter.toRoutes(reccomendationsEndpoint.serverLogic(name => IO(Right(recommendations.filter(!_.rejected)))))
+    serverInterpreter.toRoutes(
+      reccomendationsEndpoint.serverLogic(name =>
+        IO(Right(recommendations.filter(!_.rejected)))
+      )
+    )
 
   val profileRoutes: HttpRoutes[IO] =
     serverInterpreter.toRoutes(profileEndpoint.serverLogic { userId =>
@@ -132,34 +160,39 @@ object PangolinHttp4sServer extends IOApp {
         case 2 => IO(Right(selena))
         case _ => IO(Left(()))
       }
-    }
-  )
+    })
 
   val rejectProfileRoutes: HttpRoutes[IO] =
-    serverInterpreter.toRoutes(rejectProfileEndpoint.serverLogic { (userId, rejected) =>
-      userId match {
-        case 0 => {
-          tim = tim.copy(rejected = rejected)
-          IO(Right(()))
+    serverInterpreter.toRoutes(rejectProfileEndpoint.serverLogic {
+      (userId, rejected) =>
+        userId match {
+          case 0 => {
+            tim = tim.copy(rejected = rejected)
+            IO(Right(()))
+          }
+          case 1 => {
+            sally = sally.copy(rejected = rejected)
+            IO(Right(()))
+          }
+          case 2 => {
+            selena = selena.copy(rejected = rejected)
+            IO(Right(()))
+          }
+          case _ => IO(Left(()))
         }
-        case 1 => {
-          sally = sally.copy(rejected = rejected)
-          IO(Right(()))
-        }
-        case 2 => {
-          selena = selena.copy(rejected = rejected)
-          IO(Right(()))
-        }
-        case _ => IO(Left(()))
-      }
-      }
-    )
+    })
 
   override def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO]
       .withExecutionContext(ec)
       .bindHttp(8080, "0.0.0.0")
-      .withHttpApp(Router("/" -> recommendationsRoutes, "/" -> profileRoutes, "/" -> rejectProfileRoutes).orNotFound)
+      .withHttpApp(
+        Router(
+          "/" -> recommendationsRoutes,
+          "/" -> profileRoutes,
+          "/" -> rejectProfileRoutes
+        ).orNotFound
+      )
       .resource
       .useForever
 }
