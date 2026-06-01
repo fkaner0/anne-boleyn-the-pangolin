243e920ec6dcbde5a092001c228586daa50a75a4
Mystrite <finleydenton@outlook.com>
2026-06-01 15:48:15 +0100

    feat (frontend): add profile image class for bedroom wall implementation

---
+++ frontend/pangolin_app/lib/features/recommendation/domain/position.dart
@@ -0,0 +1,7 @@
+class Position {
+  final int x;
+  final int y;
+  final int rotation;
+
+  const Position({required this.x, required this.y, required this.rotation});
+}
---
+++ frontend/pangolin_app/lib/features/recommendation/domain/profile_image.dart
@@ -0,0 +1,8 @@
+import 'position.dart';
+
+class ProfileImage {
+  final String url;
+  final Position pos;
+
+  const ProfileImage({required this.url, required this.pos});
+}
