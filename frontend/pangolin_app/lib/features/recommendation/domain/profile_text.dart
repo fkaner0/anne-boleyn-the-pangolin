925de436f34cbb2a52e0433851069165f998e2ef
Mystrite <finleydenton@outlook.com>
2026-06-01 15:50:08 +0100

    feat (frontend): add profile text class for blurb-style text for bedroom wall implementation

---
+++ frontend/pangolin_app/lib/features/recommendation/domain/profile_text.dart
@@ -0,0 +1,13 @@
+import 'position.dart';
+
+class ProfileText {
+  final String title;
+  final String body;
+  final Position position;
+
+  const ProfileText({
+    required this.title,
+    required this.body,
+    required this.position,
+  });
+}
