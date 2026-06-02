9496b72e5dc993eb4986d78583bc5542da55afc7
Charlie Shaw <charliecpshaw@gmail.com>
2026-06-02 14:42:18 +0100

    chore: create tables sql file [ccs24]

---
+++ backend/pangolin-backend/tables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE profile (
+  id integer PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name text NOT NULL,
+  location text NOT NULL,
+  profileImageUrl text NOT NULL,
+);
+
+CREATE TABLE profileImage (
+  id integer PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+  userId integer NOT NULL REFERENCES profile (id) ON DELETE CASCADE,
+  url text NOT NULL,
+  x integer NOT NULL,
+  y integer NOT NULL,
+  rotation integer NOT NULL,
+  aspectRatio double precision NOT NULL,
+  scale double precision NOT NULL,
+);
+
+CREATE TABLE profileTextBox (
+  id integer PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+  userId integer NOT NULL REFERENCES profile (id) ON DELETE CASCADE,
+  title text NOT NULL,
+  body text NOT NULL,
+  x integer NOT NULL,
+  y integer NOT NULL,
+  rotation integer NOT NULL,
+  aspectRatio double precision NOT NULL,
+  scale double precision NOT NULL,
+);
