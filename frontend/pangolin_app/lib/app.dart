dfcb5ef5df2bc18a86a714915b8ed85e65f65b16
Felicity Kaner <felicity.kaner@gmail.com>
2026-06-08 05:35:55 +0100

    add basic routing structure

---
+++ frontend/pangolin_app/lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'router/app_router.dart';
+import 'theme/app_palette.dart';
+import 'theme/app_theme.dart';
+
+class PangoPalApp extends ConsumerWidget {
+  const PangoPalApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(appRouterProvider);
+    return MaterialApp.router(
+      title: 'PangoPal',
+      routerConfig: router,
+      theme: buildAppTheme(appPalette)
+    );
+  }
+}
